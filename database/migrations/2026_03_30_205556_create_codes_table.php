eca89e130620920a45c446eecfc6ac82e7f0b9db
Landy Ibrahim Ahmed <landyibrahim11@gmail.com>
2026-03-30 23:56:22 +0300

    Create migration file for codes table

---
+++ database/migrations/2026_03_30_205556_create_codes_table.php
@@ -0,0 +1,34 @@
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('codes', function (Blueprint $table) {
+            $table->id();
+            $table->string('code_name');
+            $table->unsignedBigInteger('category_id');
+            $table->timestamps();
+
+            // Foreign key constraint
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('codes');
+    }
+}
