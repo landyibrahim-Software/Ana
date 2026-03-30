f17e8ef902ff5f61a0dd87f6bca88969da66f3a3
Landy Ibrahim Ahmed <landyibrahim11@gmail.com>
2026-03-31 00:01:09 +0300

    Add code relationship method to Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1,22 +1,3 @@
-<?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class Product extends Model
-{
-    use HasFactory;
-    protected $guarded = [];
-
-    public function category(){
-        return $this->belongsTo(Category::class,'category_id','id');
+    public function code() {
+        return $this->belongsTo(Code::class, 'code_id', 'id');
     }
-
-    public function supllier(){
-        return $this->belongsTo(Supplier::class,'supplier_id','id');
-    }
-
-
-}
